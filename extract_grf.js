53d990bf37c53b869c7b128e8e5e97e165b2e234
Zili Wang <283136745@qq.com>
2025-05-17 16:54:39 +0800

    sdfsfdsdfs

---
+++ extract_grf.js
@@ -0,0 +1,32 @@
+import { GrfNode } from "@chicowall/grf-loader";
+import fs from "fs";
+import path from "path";
+
+var extract_root = path.join(process.cwd(), "data1");
+
+async function init() {
+  const filePath = path.join(process.cwd(), "data.grf");
+  console.log(filePath)
+  const fd = fs.openSync(filePath, "r");
+  var grf = new GrfNode(fd);
+  await grf.load();
+  grf.files.forEach(async (v, grf_filename , m) => {
+    // console.log("grf_filename", grf_filename)
+    var filename = grf_filename.replace(/\\/g, '\/');
+    // console.log("filename", filename)
+    const {data, error} = await grf.getFile(grf_filename);
+    var data_content= Buffer.from(data);
+    // console.log(data_content)	
+    let localPath = path.join(extract_root, filename);
+    console.log(localPath)
+    const extractDir = path.dirname(localPath);
+    if (!fs.existsSync(extractDir)) {
+      fs.mkdirSync(extractDir, { recursive: true });
+    }
+    fs.writeFileSync(localPath, data_content);
+  });
+}
+
+await init();
+
+
